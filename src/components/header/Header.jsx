069f5e4cb38fcb19fd091708fcdb423411da9392
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2023-07-09 01:16:33 +0200

    create module for header component with scss file

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
-import '../sass/main.scss';
-import { getDate } from '../lib/getDate';
+import './header.scss';
+import { getDate } from '../../lib/getDate';
 
 const Header = () => {
   const date = getDate();
