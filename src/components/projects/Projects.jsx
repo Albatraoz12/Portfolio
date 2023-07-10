3e25babfa8799fd3b0e66877e4d66d37bfbf1961
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-07-10 08:41:23 +0200

    create projects module with html structure without content

---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,15 @@
+const Projects = () => {
+  return (
+    <section className='project-section'>
+      <div className='project-container'>
+        <div className='project-card'>
+          <img src='' alt='' />
+          <h3></h3>
+          <p></p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
