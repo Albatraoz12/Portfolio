ec96f569b98678a80becaf4925175889dc61a173
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-07-10 09:30:31 +0200

    add more projects into array

--- src/lib/getDate.js
+++ src/lib/getDate.js
@@ -1,3 +1,47 @@
+// This will act as a database
+const projects = [
+  {
+    title: 'Weather App',
+    image: 'WeatherApp-ss.png',
+    status: 'Online',
+    websiteLink: 'https://stirring-profiterole-13cd77.netlify.app/',
+    githubLink: 'https://github.com/Albatraoz12/Weather-App',
+  },
+  {
+    title: 'Weather App',
+    image: 'WeatherApp-ss.png',
+    status: 'Online',
+    websiteLink: 'https://stirring-profiterole-13cd77.netlify.app/',
+    githubLink: 'https://github.com/Albatraoz12/Weather-App',
+  },
+  {
+    title: 'Weather App',
+    image: 'WeatherApp-ss.png',
+    status: 'Online',
+    websiteLink: 'https://stirring-profiterole-13cd77.netlify.app/',
+    githubLink: 'https://github.com/Albatraoz12/Weather-App',
+  },
+  {
+    title: 'Weather App',
+    image: 'WeatherApp-ss.png',
+    status: 'Online',
+    websiteLink: 'https://stirring-profiterole-13cd77.netlify.app/',
+    githubLink: 'https://github.com/Albatraoz12/Weather-App',
+  },
+  {
+    title: 'Weather App',
+    image: 'WeatherApp-ss.png',
+    status: 'Online',
+    websiteLink: 'https://stirring-profiterole-13cd77.netlify.app/',
+    githubLink: 'https://github.com/Albatraoz12/Weather-App',
+  },
+];
+
+export const fetchProjects = () => {
+  return projects;
+};
+
+//Gets the current weekday, month, date and year
 export const getDate = () => {
   const d = new Date();
   const options = {
