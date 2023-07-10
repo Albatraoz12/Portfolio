74a83e4625f3f0360b5f2cb7d8a2be27bf675d53
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2023-07-10 21:26:09 +0200

    add new description for all projects

--- src/lib/getDate.js
+++ src/lib/getDate.js
@@ -4,7 +4,7 @@ const projects = [
     title: 'Dtrain',
     image: 'DTrain-sc.png',
     description:
-      "This project represents a solo endeavor where I employed my comprehensive skills in full-stack web development. Following an agile workflow, I utilized SCRUM methodology along with gitflow, adopting a kanban board to organize and separate my sprints. With a strong emphasis on continuous integration and deployment (CI/CD), I successfully implemented the MERN stack to achieve the project's main objective: an Exercise Database with a role-based system. Users have the ability to create an account, perform CRUD operations on exercise lists, search for specific exercises, and save them to personalized lists. Additionally, the admin role encompasses all user functionalities and includes the ability to create new users directly from the dashboard.",
+      'Solo project showcasing full-stack web development skills. Used SCRUM, gitflow, and kanban for agile workflow. I used MERN stack and the project has a role system. Users can register, manage exercises, search, and save to personalized lists. Admins have user management and dashboard user creation capabilities.',
     status: 'Online',
     websiteLink: 'https://relaxed-raindrop-21bc84.netlify.app/',
     githubLink: 'https://github.com/Albatraoz12/U09-Training-App',
@@ -14,7 +14,7 @@ const projects = [
     image: 'BookOwl-ss.png',
     status: 'Online',
     description:
-      'A group project where had our teacher as a Product owner and we practise agil working (Scrum) and we used the MERN as our stack. The website lets users send their books that they wanted to loan to other people.',
+      'A group project with our teacher as the Product Owner, we practiced agile working using Scrum methodology and CI/CD. Employing the MERN stack, we developed a website that enables users to lend out their books to others or borrow a book from another user.',
     websiteLink: 'https://bookowl-u08.netlify.app/',
     githubLink:
       'https://github.com/chas-academy/u08-business-idea-team1-bokmalarna',
@@ -24,7 +24,7 @@ const projects = [
     image: 'WeatherApp-ss.png',
     status: 'Online',
     description:
-      'A webbsite created with React.js to display what weather a user location has and 5 days forward',
+      "This website was built with React.js and CSS to displays the current weather for a user's location, provides hourly weather forecasts for the next five hours, and also shows the weather forecast for the next five days.",
     websiteLink: 'https://stirring-profiterole-13cd77.netlify.app/',
     githubLink: 'https://github.com/Albatraoz12/Weather-App',
   },
@@ -33,7 +33,7 @@ const projects = [
     image: 'Recipie-ss.png',
     status: 'Online',
     description:
-      'My first solo fullstack project where I used Angular as my frontend and Laravael as my backend. For styling i used Bootstrap. Unfortunately due to Heroku shut down so is my backend anavailible. Please see github for the code :)',
+      'This is my first solo full-stack project, employing Angular for the frontend and Laravel for the backend. Bootstrap was utilized for styling. Regrettably, the backend is currently unavailable due to Heroku shutdown. Please refer to my GitHub repository for the source code.',
     websiteLink: 'https://vermillion-twilight-e4aac6.netlify.app/',
     githubLink: 'https://github.com/Albatraoz12/Frontend-Angular',
   },
@@ -42,7 +42,7 @@ const projects = [
     image: 'image-not-found.png',
     status: 'Offline',
     description:
-      'Groub project where we practise agil working (Scrum), UX and used Laravel with MySQL databse as our stack. We used Figma to design our website and build our database structure. Unfortunately due to Heroku shut down so is my backend anavailible. Please see github for the code :)',
+      'This was a group project where we practiced agile working with Scrum methodology, focused on UX. We utilized Laravel with a MySQL database as our tech stack. Figma was used for website design and creating the database structure. Regrettably, the backend is currently unavailable due to Heroku shutdown. Please refer to my GitHub repository for the source code',
     websiteLink: '',
     githubLink: 'https://github.com/Albatraoz12/u05-imdb-clone',
   },
