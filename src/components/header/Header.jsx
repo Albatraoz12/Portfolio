31d37a795f8eaf94c4ac3522f78dc0bda8d16777
Dimosthenis Emmanouil <dimothenis.emmanouil@gmail.com>
2025-05-27 20:57:43 +0200

    change profile picture

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,29 +1,29 @@
-import { getDate } from '../../lib/helperFunc';
-import './header.scss';
+import { getDate } from "../../lib/helperFunc";
+import "./header.scss";
 
 const Header = () => {
   const date = getDate();
   return (
-    <div className='hero-banner'>
-      <header className='hero-header'>
-        <span className='hero-date'>{date}</span>
-        <h1 className='hero-title'>Dimosthenis Emmanouil</h1>
-        <p className='hero-description'>
+    <div className="hero-banner">
+      <header className="hero-header">
+        <span className="hero-date">{date}</span>
+        <h1 className="hero-title">Dimosthenis Emmanouil</h1>
+        <p className="hero-description">
           Welcome to my portfolio! Step into the world of a passionate junior
           web developer, where creativity and problem-solving collide. With a
           fresh perspective and a hunger for knowledge, I&apos;m here to bring
           captivating user experiences to life and tackle complex challenges
           head-on.
         </p>
-        <a href='#project-section' className='btn'>
+        <a href="#project-section" className="btn">
           View Projects
         </a>
       </header>
-      <figure className='hero-figure'>
+      <figure className="hero-figure">
         <img
-          src='portfolio-bild.jpg'
-          alt='Desktop setup with accessories'
-          className='hero-image'
+          src="Pofilbild.jpg"
+          alt="A picture of the author/developer of this website"
+          className="hero-image"
         />
       </figure>
     </div>
